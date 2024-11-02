aaec37781377ef669a9b22b40899b20697fd47cc
KelvinErlangga <kelvinerlanggaa@gmail.com>
2024-11-02 14:42:57 +0700

    fix some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Kelvin Erlangga";
+            $("#favicon").attr("href", "assets/images/hero.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "mobile development", "web development"],
+    strings: ["frontend development", "backend development", "web development", "mobile development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
